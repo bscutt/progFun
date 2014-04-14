8ed78c9d58e04e77a2644852d643c08436869dbd
Ben Scutt <b.scutt@gmail.com>
2014-04-14 21:23:11 +0100

    Tidy up sumOfThreesAndFives exercise...

--- .worksheet/src/greeter.sumOfThreeAndFives.scala
+++ .worksheet/src/greeter.sumOfThreeAndFives.scala
@@ -1,17 +1,18 @@
 package greeter
 
-object sumOfThreeAndFives {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(316); 
-	def sumOfMultiplesThreeAndFive(upperBound: Int) = {
-		
-		def loop(f: Int => Int, acc: Int, position: Int): Int = {
-			if(position < upperBound)
-				loop(f, acc + f(position), position + 1)
-			else
-				acc
-		}
+object sumOfThreeAndFives {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(170); 
 
-		loop(x => if (x % 3 == 0 || x % 5 == 0) x else 0, 0, 0)
-	};System.out.println("""sumOfMultiplesThreeAndFive: (upperBound: Int)Int""");$skip(37); val res$0 = 
-	
-	sumOfMultiplesThreeAndFive(10000);System.out.println("""res0: Int = """ + $show(res$0))}
+  def loop(f: Int => Int)(i: Int, end: Int, acc: Int): Int = {
+    if (i < end) loop(f)(i + 1, end, acc + f(i)) else acc
+  };System.out.println("""loop: (f: Int => Int)(i: Int, end: Int, acc: Int)Int""");$skip(65); 
+
+	def divisableByThreeAndFive(x: Int) = x % 3 == 0 || x % 5 == 0;System.out.println("""divisableByThreeAndFive: (x: Int)Boolean""");$skip(65); 
+
+	def matchOrZero(f: Int => Boolean)(x: Int) = if(f(x)) x else 0;System.out.println("""matchOrZero: (f: Int => Boolean)(x: Int)Int""");$skip(127); 
+
+  def sumOfMultiplesThreeAndFive(upperBound: Int) = {
+    loop(matchOrZero(divisableByThreeAndFive)(_))(0, upperBound, 0)
+  };System.out.println("""sumOfMultiplesThreeAndFive: (upperBound: Int)Int""");$skip(37); val res$0 = 
+
+  sumOfMultiplesThreeAndFive(10000);System.out.println("""res0: Int = """ + $show(res$0))}
 }
--- src/progfun/sumOfThreeAndFives.sc
+++ src/progfun/sumOfThreeAndFives.sc
@@ -1,17 +1,20 @@
 package greeter
 
 object sumOfThreeAndFives {
-	def sumOfMultiplesThreeAndFive(upperBound: Int) = {
-		
-		def loop(f: Int => Int, acc: Int, position: Int): Int = {
-			if(position < upperBound)
-				loop(f, acc + f(position), position + 1)
-			else
-				acc
-		}
 
-		loop(x => if (x % 3 == 0 || x % 5 == 0) x else 0, 0, 0)
-	}                                         //> sumOfMultiplesThreeAndFive: (upperBound: Int)Int
-	
-	sumOfMultiplesThreeAndFive(10000)         //> res0: Int = 23331668
+  def loop(f: Int => Int)(i: Int, end: Int, acc: Int): Int = {
+    if (i < end) loop(f)(i + 1, end, acc + f(i)) else acc
+  }                                               //> loop: (f: Int => Int)(i: Int, end: Int, acc: Int)Int
+
+	def divisableByThreeAndFive(x: Int) = x % 3 == 0 || x % 5 == 0
+                                                  //> divisableByThreeAndFive: (x: Int)Boolean
+
+	def matchOrZero(f: Int => Boolean)(x: Int) = if(f(x)) x else 0
+                                                  //> matchOrZero: (f: Int => Boolean)(x: Int)Int
+
+  def sumOfMultiplesThreeAndFive(upperBound: Int) = {
+    loop(matchOrZero(divisableByThreeAndFive)(_))(0, upperBound, 0)
+  }                                               //> sumOfMultiplesThreeAndFive: (upperBound: Int)Int
+
+  sumOfMultiplesThreeAndFive(10000)               //> res0: Int = 23331668
 }
